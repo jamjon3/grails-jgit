509297d8b1acb9c4f21df975a8bb15fd0fa92b03
James Jones <jamjon3@gmail.com>
2014-09-15 16:01:38 -0400

    Defalult Userinfo handler

---
+++ src/groovy/org/grails/plugins/jgit/UserInfoHandler.groovy
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.grails.plugins.jgit
+
+/**
+ *
+ * @author james
+ */
+class UserInfoHandler {
+    /**
+     * Resolves the Git email address and uses a fallback if necessary
+     * 
+     * @param    username     The username used to resolve an email address
+     * @return                The email address associated or fallback email address if not found
+     */
+    def resolveEmail(username) {
+        if(Holders.config.jqit.fallbackMap[username]) {
+            return Holders.config.jqit.fallbackMap[username]
+        } else {
+            // use the default
+            return Holders.config.jgit.fallbackEmailDefault
+        }
+    }
+    /**
+     * Resolves the Git username and uses a fallback
+     * 
+     * @return                The username provided by fallback username
+     */
+    def resolveUsername() {
+        return Holders.config.jgit.fallbackUsername
+    }    
+}
+
