3ad32db04e7bf088105414064d510fcbb1db6cfb
James Jones <jamjon3@gmail.com>
2014-09-15 16:02:51 -0400

    Bulding out some of the basic logic

--- JgitGrailsPlugin.groovy
+++ JgitGrailsPlugin.groovy
@@ -1,3 +1,22 @@
+import org.codehaus.groovy.grails.commons.ApplicationHolder
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.api.InitCommand
+import org.eclipse.jgit.api.CloneCommand
+import org.eclipse.jgit.api.FetchCommand
+import org.eclipse.jgit.api.LsRemoteCommand
+import org.eclipse.jgit.api.PullCommand
+import org.eclipse.jgit.api.PushCommand
+import org.eclipse.jgit.api.SubmoduleSyncCommand
+import org.eclipse.jgit.api.RmCommand
+import org.eclipse.jgit.api.CheckoutCommand
+import org.eclipse.jgit.api.StatusCommand
+import org.eclipse.jgit.api.CreateBranchCommand.SetupUpstreamMode
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider
+import org.eclipse.jgit.api.errors.GitAPIException
+import grails.util.Holders
+
 class JgitGrailsPlugin {
     def version = "0.1"
     def grailsVersion = "2.2 > *"
@@ -12,25 +31,17 @@ class JgitGrailsPlugin {
     def description = '''\
 Creates a wrapper around the JGit library.
 '''
-
+    def observe = ['controllers', 'services', 'domains']
     // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/jgit"
 
-    // Extra (optional) plugin metadata
-
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
     def license = "GPL3"
-
-    // Details of company behind the plugin (if there is one)
     def organization = [ name: "University of South Florida", url: "http://www.usf.edu/" ]
 
     // Any additional developers beyond the author specified above.
 //    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
 
-    // Location of the plugin's issue tracker.
     def issueManagement = [ system: "github", url: "https://github.com/jamjon3/grails-jgit/issues" ]
-
-    // Online location of the plugin's browseable source code.
     def scm = [ url: "https://github.com/jamjon3/grails-jgit/" ]
 
     def doWithWebDescriptor = { xml ->
@@ -39,28 +50,80 @@ Creates a wrapper around the JGit library.
 
     def doWithSpring = {
         // TODO Implement runtime spring config (optional)
+        
     }
 
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
+    def doWithDynamicMethods = { ctx -> processArtifacts(application) }
+    
+    // Defined within doWithApplicationContext 
+    private Git git
+    private def credentialsProvider
+    
     def doWithApplicationContext = { applicationContext ->
         // TODO Implement post initialization spring config (optional)
+        def localGitFolder = applicationContext.getResource("web-app/git").file
+        if(localGitFolder.exists()) {
+            localRepoFolder.deleteDir()
+        }
+        FileRepositoryBuilder builder = new FileRepositoryBuilder()
+        Repository repository = builder.setGitDir(localRepoFolder)
+            .readEnvironment().findGitDir().setup().build()
+        def jgitConfig = Holders.config.jgit
+        // Load the default or custom specified user info handler
+        def userInfoHandler = this.getClass().classLoader.loadClass(jgitConfig.userInfoHandler).newInstance()
+        // Setup the Clone
+        CloneCommand clone = Git.cloneRepository()
+        // Setup the branch on the clone
+        clone.setBare(false).setBranch(jgitConfig.branch)
+        // Specify the remote uri. Ex: git@192.168.2.43:test.git OR https://github.com/someuser/SomeProject.git
+        clone.setDirectory(localRepoFolder).setURI(jgitConfig.gitRemoteURL)
+        // Specifiy username/password (only supported method for now)
+        credentialsProvider = new UsernamePasswordCredentialsProvider(jgitConfig.gitRemotelogin, jgitConfig.gitRemotePassword)
+        clone.setCredentialsProvider(credentialsProvider)
+        
+        try {
+            git = clone.call();
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+        }
+        
     }
 
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
+    
+    def onChange = { event -> processArtifacts(application) }
 
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
+    def onConfigChange = { event -> processArtifacts(application) }
 
     def onShutdown = { event ->
         // TODO Implement code that is executed when the application shuts down (optional)
+    }
+    
+    private void processArtifacts(application) {
+        def config = application.config
+        def types = config.jgit?.injectInto ?: ["Controller", "Service", "Domain"]
+        types.each { type ->
+            application.getArtefacts(type).each { klass -> addDynamicMethods(klass, application) }
+        }
+    }
+    
+    private void addDynamicMethods(klass, application) {
+        klass.metaClass.withJGit = withJGit.curry(git,credentialsProvider)
+    }
+    
+    private withJGit = { Git git, def credentialsProvider, Closure closure ->
+        PushCommand pushcm = git.push()
+        pushcm.setCredentialsProvider(credentialsProvider)
+        pushcm.setRemote(Holders.config.jgit.gitRemoteURL)
+        PullCommand pullcm = git.pull()
+        pullcm.setCredentialsProvider(user)
+        // pull.setRemote(Holders.config.jgit.gitRemoteURL)
+        RmCommand rmcm = git.rm()        
+        SubmoduleSyncCommand synccm = git.submoduleSync()
+        
+        if (closure) {
+            closure.delegate = git
+            closure.resolveStrategy = Closure.DELEGATE_FIRST
+            closure()
+        }
     }
 }
