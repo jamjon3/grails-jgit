4233e0b835af04a3dc03b3888f3750163c6447b9
James Jones <jamjon3@gmail.com>
2014-09-16 13:28:58 -0400

    Removed config file

--- src/resources/JGitConfig.groovy
+++
@@ -1,9 +0,0 @@
-jgit.userInfoHandler = 'org.grails.plugins.jgit.UserInfoHandler'
-jgit.fallbackMap = [:]
-jgit.fallbackEmailDefault = 'jdoe@foo.net'
-jgit.fallbackUsername = 'jdoe'
-jgit.branch = 'master'
-jgit.gitRemoteURL = 'https://github.com/someuser/SomeApp.git'
-jgit.gitRemotelogin = 'jdoe'
-jgit.gitRemotePassword = 'mygitpassword'
-jgit.injectInto = ["Controller", "Service","Domain"]
