7dd8c52dcf0a0f0fd1f50203b8710df430501cea
James Jones <jamjon3@gmail.com>
2014-09-24 09:47:57 -0400

    Removed extra ")"'s

--- scripts/_Install.groovy
+++ scripts/_Install.groovy
@@ -1,7 +1,7 @@
 // ensure that there are 'git' directory under application 'web-app'
 ant.mkdir(dir:"${basedir}/web-app/git")
 def gitignoreFile = new File(basedir, '.gitignore')
-if (!gitignoreFile.exists() ? true : gitignoreFile.readLines().findAll { it.contains("web-app/git") }.isEmpty() )) {
+if (!gitignoreFile.exists() ? true : gitignoreFile.readLines().findAll { it.contains("web-app/git") }.isEmpty() ) {
     gitignoreFile.withWriterAppend {
         it.writeLine "/web-app/git/"
     }
