fa9cf5706b74bf52071ac28ed615d14a1af779a8
James Jones <jamjon3@gmail.com>
2014-10-16 11:41:31 -0400

    Updated the http prefix for http parameters

--- scripts/JgitConfig.groovy
+++ scripts/JgitConfig.groovy
@@ -24,8 +24,8 @@ jgit.fallbackEmailDefault = 'jdoe@foo.net'
 jgit.fallbackUsername = 'jdoe'
 jgit.branch = 'master'
 jgit.gitRemoteURL = 'https://github.com/someuser/SomeApp.git'
-jgit.gitRemotelogin = 'jdoe'
-jgit.gitRemotePassword = 'mygitpassword'
+jgit.http.gitRemotelogin = 'jdoe'
+jgit.http.gitRemotePassword = 'mygitpassword'
 jgit.injectInto = ['Controller', 'Service','Domain']
 '''
         }
